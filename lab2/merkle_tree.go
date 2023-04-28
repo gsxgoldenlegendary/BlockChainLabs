af4c1c0875c34bc19778a54f7529e79dd7718fb0
Songxiao Guo <logname@mail.ustc.edu.cn>
2023-04-28 09:24:38 +0800

    complete func `NewMerkleTree`.

--- lab2/merkle_tree.go
+++ lab2/merkle_tree.go
@@ -1,6 +1,9 @@
 package main
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"math"
+)
 
 // MerkleTree represent a Merkle tree
 type MerkleTree struct {
@@ -18,8 +21,29 @@ type MerkleNode struct {
 // NewMerkleTree creates a new Merkle tree from a sequence of data
 // TODO
 func NewMerkleTree(data [][]byte) *MerkleTree {
-
-	return nil
+	if len(data)%2 == 1 {
+		data = append(data, data[len(data)-1])
+	}
+	var nodes []*MerkleNode
+	for _, datum := range data {
+		node := NewMerkleNode(nil, nil, datum)
+		nodes = append(nodes, node)
+	}
+	length := len(nodes)
+	index := 0
+	for length > 1 {
+		var level []*MerkleNode
+		for index = 0; index < length; index += 2 {
+			if index+1 == length {
+				level = append(level, NewMerkleNode(nodes[index], nodes[index], nil))
+				break
+			}
+			level = append(level, NewMerkleNode(nodes[index], nodes[index+1], nil))
+		}
+		nodes = level
+		length = int(math.Ceil(float64(length) / 2))
+	}
+	return &MerkleTree{nodes[0], data}
 }
 
 // NewMerkleNode creates a new Merkle tree node
