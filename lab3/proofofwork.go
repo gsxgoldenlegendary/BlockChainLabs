9c709d953e1d20825f268ee4483393bacd845ff5
Songxiao Guo <logname@mail.ustc.edu.cn>
2023-06-03 15:32:36 +0800

    finish `Run` `Validate` .

--- lab3/proofofwork.go
+++ lab3/proofofwork.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"math"
 	"math/big"
 )
@@ -30,13 +32,51 @@ func NewProofOfWork(b *Block) *ProofOfWork {
 // Run performs a proof-of-work
 // implement
 func (pow *ProofOfWork) Run() (int64, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
 	nonce := int64(0)
 
-	return nonce, nil
+	for nonce < int64(maxNonce) {
+		data := bytes.Join(
+			[][]byte{
+				IntToHex(pow.block.Header.Version),
+				pow.block.Header.PrevBlockHash[:],
+				pow.block.Header.MerkleRoot[:],
+				IntToHex(pow.block.Header.Timestamp),
+				IntToHex(targetBits),
+				IntToHex(nonce),
+			},
+			[]byte{},
+		)
+		hash = sha256.Sum256(data)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+
+	return nonce, hash[:]
 }
 
 // Validate validates block's PoW
 // implement
 func (pow *ProofOfWork) Validate() bool {
-	return true
+	data := bytes.Join(
+		[][]byte{
+			IntToHex(pow.block.Header.Version),
+			pow.block.Header.PrevBlockHash[:],
+			pow.block.Header.MerkleRoot[:],
+			IntToHex(pow.block.Header.Timestamp),
+			IntToHex(targetBits),
+			IntToHex(pow.block.Header.Nonce),
+		},
+		[]byte{},
+	)
+	hash := sha256.Sum256(data)
+	var hashInt big.Int
+	hashInt.SetBytes(hash[:])
+	return hashInt.Cmp(pow.target) == -1
 }
